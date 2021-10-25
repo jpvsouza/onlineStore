7d8a6ba8d4a2feb46d7f9208b02dc623bbd3b8b9
Vitor Diorio <vitor.diorio@yahoo.com.br>
2021-10-25 17:12:57 -0300

    Correções de lint e comentarios para melhor entedimento do cóodigo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,21 +5,24 @@ import ProductCard from './ProductCard';
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
+    // Criação dos estados iniciais da página
     this.state = {
-      searched: false,
       search: '',
       isLoading: false,
       products: [],
     };
   }
 
+  // Função que atualiza o estado do input de busca
   handleChange = (e) => {
     this.setState({
       search: e.target.value,
     });
   }
 
-  handleClick = () => {
+  // Função de requisição da API pelo clique no botão de pesquisar
+  handleClick = (e) => {
+    e.preventDefault();
     const { search } = this.state;
     this.setState({ isLoading: true });
     getProductsFromCategoryAndQuery('', search)
@@ -27,27 +30,32 @@ export default class Home extends React.Component {
   }
 
   render() {
-    const { searched, search, isLoading, products } = this.state;
-    // Versão inicial da pagina home, apenas cumprindo o requisito 2
+    const { isLoading, products } = this.state;
     return (
       <div>
-        <input data-testid="query-input" type="text" onChange={ this.handleChange } />
-        <button data-testid="query-button" type="button" onClick={ this.handleClick }>
-          Pesquisar
-        </button>
+        <form action="">
+          <input data-testid="query-input" type="text" onChange={ this.handleChange } />
+          <button data-testid="query-button" type="button" onClick={ this.handleClick }>
+            Pesquisar
+          </button>
+        </form>
         <h1 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h1>
         <div className="product-container">
-          { isLoading && <h1>Carregando...</h1> }
-          { products.length > 0 && products.map((product) => (
-            <ProductCard
-              key={ product.id }
-              title={ product.title }
-              price={ product.price }
-              thumbnail={ product.thumbnail }
-            />
-          )) }
+          { // Mostra a mensagem de carregando enquanto a api é requisitada
+            isLoading && <h1>Carregando...</h1>
+          }
+          { // Chama o card de produtos para cada item na resposta da API
+            products.length > 0 && products.map((product) => (
+              <ProductCard
+                key={ product.id }
+                title={ product.title }
+                price={ product.price }
+                thumbnail={ product.thumbnail }
+              />
+            ))
+          }
         </div>
       </div>
     );
